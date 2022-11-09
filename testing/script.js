2690f61844573331650fb95eb2ba9e8e21be7df7
Gavin <waltersg298@gmail.com>
2022-11-08 19:08:37 -0700

    updated style and added a rotation behavior to event

--- testing/script.js
+++ testing/script.js
@@ -1,3 +1,4 @@
+'use strict';
 
 let string = "something";
 let size = 50;
@@ -7,7 +8,18 @@ document.querySelector('h1').innerText = `This is going to be ${string}`;
 
 document.querySelector('body').addEventListener('click', () => {
     size--;
-    rotation += 10;
+    rotation += 25;
     document.querySelector('h1').style.fontSize = `${size}px`; 
-    document.querySelector('h1').style.transform = `rotate(${rotation}deg)`;;
+    document.querySelector('h1').style.transitionDuration = `1s`; 
+    document.querySelector('h1').style.transform = `rotate(${rotation}deg)`;
 })
+
+function printSomething() {
+    let string = "hello"; 
+
+    console.log(string); 
+
+    console.log(this.string); 
+}
+
+printSomething();
