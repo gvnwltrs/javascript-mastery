1235849ef73d38f32ae4ac7fbe0782d3e29d625c
Gavin <waltersg298@gmail.com>
2022-11-10 18:33:59 -0700

    destructuring arrays

--- testing/script.js
+++ testing/script.js
@@ -31,6 +31,28 @@ let anotherPerson = person;
 
 person.firstName = 'Jules';
 copyPerson.firstName = 'Lahey';
+anotherPerson.firstName = 'Dorian';
 
 console.log(person); 
 console.log(copyPerson); 
+console.log(anotherPerson); 
+
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy', 
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'], 
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic', 'Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto']
+}; 
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1]; 
+const c = arr[2]; 
+
+const [x, y, z] = arr; 
+console.log(x, y, z); 
+
+const [e, g] = arr;
+console.log(e, g); 
