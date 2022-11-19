7f4d6be284c206dece4a4070646144d273570814
Gavin <waltersg298@gmail.com>
2022-11-18 18:59:27 -0700

    added map layer

--- MaptyApp/script.js
+++ MaptyApp/script.js
@@ -15,7 +15,24 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         const {latitude} = position.coords;
         const {longitude} = position.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+        console.log(typeof(latitude));
+        console.log(typeof(longitude));
+
+        const coords = [latitude, longitude]; 
+
+        console.log(coords); 
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     }, function() {
         alert('could not get your position')
     })
