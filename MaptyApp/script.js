cca4b0819e84118ff9c5724e746e9cc4c1da5565
Gavin <waltersg298@gmail.com>
2022-11-18 19:05:44 -0700

    vpn issue comment

--- MaptyApp/script.js
+++ MaptyApp/script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// you vpn will affect where you end up in you leaflet map
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         const {latitude} = position.coords;
