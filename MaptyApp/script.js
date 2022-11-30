fc0baa23a80a51679202446038a4f2a5233be11b
Gavin <waltersg298@gmail.com>
2022-11-30 15:18:53 -0700

    issues with click()

--- MaptyApp/script.js
+++ MaptyApp/script.js
@@ -27,7 +27,8 @@ class Workout {
         const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
         'August', 'September', 'October', 'November', 'December'];
 
-        this.description = `${this.type[0].toUpperCase() + this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
+        this.description = `${this.type[0].toUpperCase() + this.type.slice(1)} 
+            on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
     }
 
     _click() {
