a8fde3b476a82cd977adb1fa7437332e1fa1585d
Gavin <waltersg298@gmail.com>
2022-12-01 18:50:55 -0700

    pulling public data with public api for global regions

--- async-code/script.js
+++ async-code/script.js
@@ -10,5 +10,24 @@ request.open('GET', 'https://restcountries.com/v3.1/name/united');
 request.send();
 
 request.addEventListener('load', function(){
-    console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText); 
+    console.log(data);
+
+    const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+          <h3 class="country__name">${Object.values(data.name)[0]}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[Object.keys(data.languages)[0]]}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[Object.keys(data.currencies)[0]].name}</p>
+        </div>
+      </article> 
+    `; 
+
+    countriesContainer.insertAdjacentHTML('beforeend', 
+    html);
+    countriesContainer.style.opacity = 1; 
+
 });
