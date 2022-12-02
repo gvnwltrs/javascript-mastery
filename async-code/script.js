a718bfa0615da47201bafb1126f6987073c4c704
Gavin <waltersg298@gmail.com>
2022-12-01 18:57:18 -0700

    now getting coutry data and displaying it

--- async-code/script.js
+++ async-code/script.js
@@ -5,29 +5,38 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
+const getCountryData = function(country) {
+
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/united');
-request.send();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-request.addEventListener('load', function(){
-    const [data] = JSON.parse(this.responseText); 
-    console.log(data);
+  request.addEventListener('load', function(){
+      const [data] = JSON.parse(this.responseText); 
+      console.log(data);
 
-    const html = `
-      <article class="country">
-        <img class="country__img" src="${data.flags.png}" />
-        <div class="country__data">
-          <h3 class="country__name">${Object.values(data.name)[0]}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
-          <p class="country__row"><span>🗣️</span>${data.languages[Object.keys(data.languages)[0]]}</p>
-          <p class="country__row"><span>💰</span>${data.currencies[Object.keys(data.currencies)[0]].name}</p>
-        </div>
-      </article> 
-    `; 
+      const html = `
+        <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${Object.values(data.name)[0]}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[Object.keys(data.languages)[0]]}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[Object.keys(data.currencies)[0]].name}</p>
+          </div>
+        </article> 
+      `; 
 
-    countriesContainer.insertAdjacentHTML('beforeend', 
-    html);
-    countriesContainer.style.opacity = 1; 
+      countriesContainer.insertAdjacentHTML('beforeend', 
+      html);
+      countriesContainer.style.opacity = 1; 
 
-});
+  });
+}; 
+
+getCountryData('portugal'); 
+getCountryData('usa'); 
+getCountryData('mexico'); 
+getCountryData('philippines'); 
+getCountryData('china'); 
